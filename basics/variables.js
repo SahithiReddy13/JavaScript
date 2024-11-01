05ebf4672efc614207c28e9019c04129e160f5f9
SahithiReddy13 <sahithireddyy06@gmail.com>
2024-11-01 14:54:02 +0000

    commit

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accNo=12345
+let accName="Sahithi"
+var accEmail="sahithi@gmail.com"
+accPass="sahithi"
+let accStatus
+// accNo=54321
+// we dont use var due to block and functional scope
+console.log(accNo)
+accName="SahithiReddy"
+accEmail="sahithireddy@gmail.com"
+accPass="sahithireddy"
+console.table([accNo,accName,accEmail,accPass,accStatus])
+
