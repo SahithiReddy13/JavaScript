9b1a87ecf01a527676aa96f6b97cdf4c92a55425
SahithiReddy13 <sahithireddyy06@gmail.com>
2024-11-01 15:57:44 +0000

    commit

---
+++ basics/conversion.js
@@ -0,0 +1,29 @@
+let age= "22a"
+console.log(typeof age);
+console.log(typeof (age));
+let changedAge=Number(age)
+console.log(typeof (changedAge));
+console.log(changedAge);
+
+
+let age1= undefined
+console.log(typeof age1);
+console.log(typeof (age1));
+let changedAge1=Number(age1)
+console.log(typeof (changedAge1));
+console.log(changedAge1);
+
+/* to Number
+"33"-33
+"33abc"-NaN
+true-1,false-0
+null-0
+undefined-NaN
+*/
+/* to Boolean
+""-false
+"sahithi"-true
+*/
+
+
+
