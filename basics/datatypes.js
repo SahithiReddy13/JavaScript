b2806dc962ceae73bac5589e6ec4daa60231c0ae
SahithiReddy13 <sahithireddyy06@gmail.com>
2024-11-01 15:04:12 +0000

    commit

---
+++ basics/datatypes.js
@@ -0,0 +1,11 @@
+"use strict "; //for new version of JS
+// alert(3+5) //alert is not defined
+/*code readability should be high and refer mdn & tc39.es
+ number- 2 to the power of 53
+ boolean- true/false
+ string-""
+ bigint-for larger values
+ undefined-for variables declared and not assigned values,type is undefined
+ null-standalone value,type is object
+ symbol-unique
+ object */
