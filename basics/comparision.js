bdd1ed66e83fdd3b44b5473da57e2d70792ae7ae
SahithiReddy13 <sahithireddyy06@gmail.com>
2024-11-02 01:38:04 +0000

    commit

---
+++ basics/comparision.js
@@ -0,0 +1,28 @@
+// console.log(5>3);
+// console.log(5<3);
+// console.log(5>=3);
+// console.log(2<=5);
+// console.log(5==3);
+// console.log(5!=3);
+
+console.log("5">3);
+console.log("05">=3);
+
+console.log(null>0);//equality check == and comparision >,<,>=,<= operators works differently
+console.log(null==0);//comparision operators convert null to 0
+console.log(null>=0);//true 0 >=0
+ 
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined>=0)// false in all 3 cases
+//=== strict check- checks value and datatype
+console.log("3"===3);//false
+
+
+
+
+
+
+
+
+
