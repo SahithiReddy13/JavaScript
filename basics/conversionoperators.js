755d29bc748d95f52cd29cd9cf8546bd76cd8237
SahithiReddy13 <sahithireddyy06@gmail.com>
2024-11-02 01:10:48 +0000

    commit

---
+++ basics/conversionoperators.js
@@ -0,0 +1,70 @@
+let age= "22a"
+console.log(typeof age);
+console.log(typeof (age));
+let changedAge=Number(age)
+console.log(typeof (changedAge));
+console.log(changedAge);
+
+
+let age1= undefined
+console.log(typeof age1);
+console.log(typeof (age1));
+let changedAge1=Number(age1)
+console.log(typeof (changedAge1));
+console.log(changedAge1);
+
+/* to Number
+"33"-33
+"33abc"-NaN
+true-1,false-0
+null-0
+undefined-NaN
+*/
+/* to Boolean
+""-false
+"sahithi"-true
+*/
+
+// Operators
+let val =3
+let negVal=-val
+console.log(negVal);
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**4)
+console.log(2/2)
+console.log(5%3);
+let str1= "hello"
+let str2=" world"
+let str3=str1+str2
+console.log(str3);
+console.log(1+"2")
+console.log("1"+2)
+console.log("1"+2+3)
+console.log(1+2+"3");
+console.log(+true)
+// console.log(true+);
+console.log(+"");
+let n1,n2,n3
+n1=n2=n3=2+2
+console.log(n1);
+let score=10
+score++
+console.log(score);
+let score1= --score
+console.log(score1);
+console.log(2+2*3%6); //all these are not used due to readability issues
+
+
+
+
+
+
+
+
+
+
+
+
